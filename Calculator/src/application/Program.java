92e69c3e205d306b0e3ec4858a744d53ef0fd4f1
JoaoPedroBackEndDev <joaopedroacostaalvarez@gmail.com>
2025-08-14 11:34:19 -0300

    Add files via upload

---
+++ Calculator/src/application/Program.java
@@ -0,0 +1,39 @@
+package application;
+
+import model.entities.Calculator;
+
+import java.util.InputMismatchException;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main (String[] args){
+
+        Calculator cal = new Calculator();
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        try{
+        System.out.print("Enter a number: ");
+        double number1 = sc.nextInt();
+
+        System.out.print("(+,-,x,/): ");
+        char operator = sc.next().charAt(0);
+
+        System.out.print("Enter a second number: ");
+        double number2 = sc.nextInt();
+
+
+            double result = cal.Calculate(number1,number2,operator);
+            System.out.printf("Result: %.2f", result);
+        }catch (InputMismatchException e){
+            System.out.println("Error: Not accepted input");
+        }catch (IllegalArgumentException e){
+            System.out.println("Error! " + e.getMessage());
+        }catch (ArithmeticException e){
+            System.out.println("Error: " + e.getMessage());
+        }
+        sc.close();
+    }
+}
